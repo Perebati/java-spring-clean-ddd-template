49f2f9aeb3311dc211f24312d5d6873f0837e6d2
perebati <lucas.bpereira1999@gmail.com>
2025-07-02 11:20:01 -0300

    [FEATURE]: (02/07/2025) Nova classe Person.

---
+++ src/main/java/org/gfinnovation/dealsafe/modules/tree/_shared/infrastructure/Person.java
@@ -0,0 +1,8 @@
+package org.gfinnovation.dealsafe.modules.tree._shared.infrastructure;
+
+import jakarta.persistence.Entity;
+import org.gfinnovation.dealsafe._shared.infrastructure.GenericBusinessEntity;
+
+@Entity
+public class Person extends GenericBusinessEntity {
+}
